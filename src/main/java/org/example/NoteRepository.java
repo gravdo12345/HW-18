5605b9e79e83f4ffd45e25c2c1a8f740f67b855c
GRAVDO <gravdo.d@gmail.com>
2024-03-22 20:46:21 +0100

    "some diabolical message"

--- src/main/java/org/example/NoteRepository.java
+++ src/main/java/org/example/NoteRepository.java
@@ -3,4 +3,7 @@ package org.example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    // Method to update a note
+    Note saveAndFlush(Note note);
+    void deleteById(Long id);
 }
