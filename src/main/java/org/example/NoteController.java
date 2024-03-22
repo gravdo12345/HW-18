84ae0deb1fd7e41e0637c43a1999f57c943bbbf8
GRAVDO <gravdo.d@gmail.com>
2024-03-22 21:15:13 +0100

    "some diabolical message"

--- src/main/java/org/example/NoteController.java
+++ src/main/java/org/example/NoteController.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,13 +16,26 @@ public class NoteController {
     private NoteService noteService;
 
     @GetMapping
-    public List<Note> getAllNotes() {
-        return noteService.getAllNotes();
+    public ResponseEntity<List<Note>> getAllNotes() {
+        List<Note> notes = noteService.getAllNotes();
+        return new ResponseEntity<>(notes, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
+        Note note = noteService.getNoteById(id);
+        return new ResponseEntity<>(note, HttpStatus.OK);
     }
 
     @PostMapping
-    public Note createNote(@Valid @RequestBody Note note) {
-        return noteService.createNote(note);
+    public ResponseEntity<Note> createOrUpdateNote(@Valid @RequestBody Note note) {
+        Note createdNote = noteService.createOrUpdateNote(note);
+        return new ResponseEntity<>(createdNote, HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNoteById(@PathVariable Long id) {
+        noteService.deleteNoteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/org/example/NoteService.java
+++ src/main/java/org/example/NoteService.java
@@ -15,9 +15,18 @@ public class NoteService {
         return noteRepository.findAll();
     }
 
-    public Note createNote(Note note) {
+    public Note getNoteById(Long id) {
+        return noteRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Note with id " + id + " not found"));
+    }
+
+    public Note createOrUpdateNote(Note note) {
         return noteRepository.save(note);
     }
 
+    public void deleteNoteById(Long id) {
+        noteRepository.deleteById(id);
+    }
 }
 
+// have you ever read lovecraft? this shi is horrific, can't sleep no more.
